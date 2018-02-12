7f7698c91d6f744b37c5ebb5c6d732233ab09504
Curtis Cawley <curtis.cawley@icloud.com>
2018-02-12 15:57:04 -0800

    created a working getAll dogs function

--- model/index.js
+++ model/index.js
@@ -1,5 +1,14 @@
+// let uuid = require('uuid/v4');
+// let fs = require('fs');
+// let filePath = './data.json';
+// let dogsInJSON = fs.readFileSync('./data.json', 'utf-8');
+// let dogs = JSON.parse(booksInJSON)
+let knex = require('../db');
 
+function getAll() {
+  return knex('dogs');
+}
 
 module.exports = {
-
+  getAll
 }
