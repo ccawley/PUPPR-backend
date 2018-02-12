da549dba086bd3729460db8d6f282aeafa72aed1
Curtis Cawley <curtis.cawley@icloud.com>
2018-02-12 14:44:41 -0800

    basic express server setup and tested working

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express")
 const app = express()
-const model = require('./model')
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
+const bodyParser = require("body-parser")
+const morgan = require("morgan")
+const model = require("./model")
+// const controller = require("./controller")
 
-app.use(bodyParser.json())
 app.disable('x-powered-by')
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"))
+app.use(bodyParser.json())
 
-app.get('/dogs', req, res => {
+app.get('/dogs', (req, res) => {
   let message = "Woof"
   console.log(`I am server, ${message}!`)
   res.send(`You are client! Boo! Heck! ${message}!`)
 })
 
-app.listen(PORT, () => {
-  console.log(`App listening on ${PORT}`)
+app.use((req, res, next) => {
+  res.status(404).json({ error: { message: 'Not found' }})
 })
+
+app.use((err, req, res, next) => {
+  let status = err.status || 500
+  console.log(err)
+  res.status(status).json({ error: err })
+})
+
+let listener = () => `Listening on port ${PORT}!`
+app.listen(PORT, listener)
+
+module.exports = app
