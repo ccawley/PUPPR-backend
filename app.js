1bfc61df2070233ec74bbbd0e0d060622141308a
Curtis Cawley <curtis.cawley@icloud.com>
2018-02-12 10:24:39 -0800

    created model folder, touched index.js file in it, updated app.js file to require the new index file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const model = require('./model')
 const PORT = process.env.port || 3000
 app.use(bodyParser.json())
 app.disable('x-powered-by')
