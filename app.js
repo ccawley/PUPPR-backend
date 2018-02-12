cc534998c13f229dd44d9ffbf7532787df14633e
Curtis Cawley <curtis.cawley@icloud.com>
2018-02-12 14:32:27 -0800

    added a test get route

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const bodyParser = require('body-parser')
 const app = express()
 const model = require('./model')
 const PORT = process.env.port || 3000
+
 app.use(bodyParser.json())
 app.disable('x-powered-by')
+
+app.get('/dogs', req, res => {
+  let message = "Woof"
+  console.log(`I am server, ${message}!`)
+  res.send(`You are client! Boo! Heck! ${message}!`)
+})
 
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT}`)
